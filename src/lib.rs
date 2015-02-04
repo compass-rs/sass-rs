dbb9709f71857c13350ef7d6577c2efdcd72c3b3
winding-lines <github@seritan.net>
2015-02-03 21:26:19 -0800

    Add safer wrapper SassImporter

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,25 @@ extern crate libc;
 use libc::c_char;
 use std::{ffi,str};
 
+pub struct SassImporter {
+  pub callback: sass_sys::Sass_C_Import_Callback
+}
+
+impl SassImporter {
+  pub fn new(arg1: sass_sys::Sass_C_Import_Fn,
+    cookie: *mut ::libc::c_void) -> SassImporter {
+      SassImporter { callback:
+        unsafe{sass_sys::sass_make_importer(arg1,cookie)}
+      }
+  }
+}
+
+impl Drop for SassImporter {
+  fn drop(&mut self) {
+    unsafe { sass_sys::sass_delete_importer(self.callback) }
+  }
+}
+
 fn build_string(c_buf:*const c_char) -> String {
   let buf: &[u8] = unsafe { ffi::c_str_to_bytes(&c_buf) };
   let str_slice: &str = str::from_utf8(buf).unwrap();
