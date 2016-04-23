3606396d5ad4e0c83839a2632a6bb8be7ff114a7
Rasmus Kaj <kaj@kth.se>
2016-04-23 20:06:47 +0200

    Add API for specifying output style.
    Without this, raw sass_sys need to be used for specifying output
    style.
    
    The compile_sass example is changed to output two different styles.

--- examples/compile_sass.rs
+++ examples/compile_sass.rs
@@ -1,7 +1,7 @@
 /// Example file on how to compile a scss file.
 
 extern crate sass_rs;
-use sass_rs::sass_context::SassFileContext;
+use sass_rs::sass_context::{SassFileContext, OutputStyle};
 use sass_rs::sass_function::*;
 use sass_rs::sass_value::*;
 use sass_rs::dispatcher::Dispatcher;
@@ -19,18 +19,20 @@ impl SassFunction for Foo {
 }
 
 /// Setup the environment and compile a file.
-fn compile(filename:&str) {
+fn compile(filename:&str, style:OutputStyle) {
     let mut file_context = SassFileContext::new(filename);
     let foo = Foo;
     let fns:Vec<(&'static str,Box<SassFunction>)> = vec![("foo($x)", Box::new(foo))];
     let options = file_context.sass_context.sass_options.clone();
+    options.write().ok().unwrap().set_output_style(style.clone());
     thread::spawn(move|| {
         let dispatcher = Dispatcher::build(fns,options);
         while dispatcher.dispatch().is_ok() {}
     });
     let out = file_context.compile();
     match out {
-        Ok(css) => println!("------- css  ------\n{}\n--------", css),
+        Ok(css) => println!("------- css ({:?}) ------\n{}--------",
+                            style, css),
         Err(err) => println!("{}", err)
     };
 }
@@ -40,5 +42,6 @@ pub fn main() {
     let _ = args.next();
     let file = args.next().expect("Please pass in a file name");
     println!("Compiling sass file: `{}`.", file);
-    compile(&file);
+    compile(&file, OutputStyle::Expanded);
+    compile(&file, OutputStyle::Compressed);
 }
